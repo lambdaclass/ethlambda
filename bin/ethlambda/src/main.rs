1ea2a75d13142ed449047f9940493f33b3c2d3f8
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-12-23 18:09:51 -0300

    feat: deserialize genesis configuration

--- bin/ethlambda/src/main.rs
+++ bin/ethlambda/src/main.rs
@@ -1,3 +1,7 @@
+use clap::Parser;
+use ethereum_types::{H256, U256};
+use serde::{Deserialize, Serialize};
+
 const ASCII_ART: &str = r#"
       _   _     _                 _         _
   ___| |_| |__ | | __ _ _ __ ___ | |__   __| | __ _
@@ -6,6 +10,19 @@ const ASCII_ART: &str = r#"
  \___|\__|_| |_|_|\__,_|_| |_| |_|_.__/ \__,_|\__,_|
 "#;
 
+#[derive(Debug, clap::Parser)]
+struct CliOptions {
+    #[arg(long)]
+    custom_genesis_json_file: String,
+}
+
 fn main() {
+    let options = CliOptions::parse();
     println!("{ASCII_ART}");
+    let genesis_json = std::fs::read_to_string(&options.custom_genesis_json_file)
+        .expect("Failed to read genesis.json");
+    let genesis: Genesis =
+        serde_json::from_str(&genesis_json).expect("Failed to parse genesis.json");
+
+    println!("Shutting down...");
 }
