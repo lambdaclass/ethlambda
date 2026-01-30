5e11024f72ba52714116a69ed2b306e4d37692fe
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-01-30 18:08:28 -0300

    chore: fmt

--- crates/net/p2p/src/req_resp/mod.rs
+++ crates/net/p2p/src/req_resp/mod.rs
@@ -7,6 +7,6 @@ pub use codec::Codec;
 pub use encoding::MAX_COMPRESSED_PAYLOAD_SIZE;
 pub use handlers::{build_status, fetch_block_from_peer, handle_req_resp_message};
 pub use messages::{
-    BLOCKS_BY_ROOT_PROTOCOL_V1, BlocksByRootRequest, Request, RequestedBlockRoots, Response, ResponseCode,
-    ResponsePayload, STATUS_PROTOCOL_V1, Status, error_message,
+    BLOCKS_BY_ROOT_PROTOCOL_V1, BlocksByRootRequest, Request, RequestedBlockRoots, Response,
+    ResponseCode, ResponsePayload, STATUS_PROTOCOL_V1, Status, error_message,
 };
