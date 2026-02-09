4dc2521642280e2b6c996950b5b22ca657319a79
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-02-09 14:22:07 -0300

    feat: add support for "ip6" too

--- crates/net/p2p/src/lib.rs
+++ crates/net/p2p/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::{HashMap, HashSet},
-    net::{IpAddr, Ipv4Addr, SocketAddr},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
     time::Duration,
 };
 
@@ -459,14 +459,25 @@ pub fn parse_enrs(enrs: Vec<String>) -> Vec<Bootnode> {
 
         let quic_port = u16::decode(quic_port_bytes.as_ref()).unwrap();
 
-        // TODO: support IPv6
-        let (_, ip_bytes) = record
+        let ipv4 = record
             .pairs
             .iter()
             .find(|(key, _)| key.as_ref() == b"ip")
+            .map(|(_, bytes)| {
+                let octets: [u8; 4] = bytes.as_ref().try_into().expect("invalid IPv4 address");
+                IpAddr::from(Ipv4Addr::from(octets))
+            });
+        let ipv6 = record
+            .pairs
+            .iter()
+            .find(|(key, _)| key.as_ref() == b"ip6")
+            .map(|(_, bytes)| {
+                let octets: [u8; 16] = bytes.as_ref().try_into().expect("invalid IPv6 address");
+                IpAddr::from(Ipv6Addr::from(octets))
+            });
+        let ip = ipv4
+            .or(ipv6)
             .expect("node record missing IP address");
-        let ip_octets: [u8; 4] = ip_bytes.as_ref().try_into().expect("invalid IPv4 address");
-        let ip = IpAddr::from(Ipv4Addr::from(ip_octets));
 
         bootnodes.push(Bootnode {
             ip,
