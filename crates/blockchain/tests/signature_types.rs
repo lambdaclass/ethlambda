850dd832cee83a2e0805fba26db15b33febe0376
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2026-02-25 17:59:38 -0300

    fix imports

--- crates/blockchain/tests/signature_types.rs
+++ crates/blockchain/tests/signature_types.rs
@@ -1,9 +1,8 @@
 use super::common::{AggregationBits, Block, Container, ProposerAttestation, TestInfo, TestState};
-use ethlambda_types::attestation::XmssSignature;
 use ethlambda_types::attestation::{AggregationBits as EthAggregationBits, XmssSignature};
 use ethlambda_types::block::{
-    AggregatedSignatureProof, AggregationBits as EthAggregationBitsSig, AttestationSignatures,
-    BlockSignatures, BlockWithAttestation, SignedBlockWithAttestation,
+    AggregatedSignatureProof, AttestationSignatures, BlockSignatures, BlockWithAttestation,
+    SignedBlockWithAttestation,
 };
 use ethlambda_types::primitives::ssz::{Decode as SszDecode, Encode as SszEncode};
 use serde::Deserialize;
