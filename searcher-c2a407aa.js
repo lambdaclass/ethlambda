8751b9f46b03a3b1f3bf148777f6aafcc04783b6
pablodeymo <pablodeymo@users.noreply.github.com>
2026-06-01 19:20:37 +0000

    deploy: 7c3cd6aea8e289b40452b45b53642c7fe48ff24c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eac69af8.js',
+                path_to_root + 'searchindex-16a9b1e4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
