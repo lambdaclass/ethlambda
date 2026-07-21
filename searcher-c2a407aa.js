a392228a29178448e7476eb84f703b3bc43ed2bd
MegaRedHand <MegaRedHand@users.noreply.github.com>
2026-07-21 20:01:18 +0000

    deploy: b4ed4797589fa5394782c75898c6a061d76fae43

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-beb77040.js',
+                path_to_root + 'searchindex-fd1243a6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
